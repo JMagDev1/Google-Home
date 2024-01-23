8cdac43d67d18cba6f922c368389204277a95f2e
Jack <112323010+JMagDev1@users.noreply.github.com>
2024-01-23 11:32:41 +0000

    Working on light and dark mode. Works for 1 link and time.

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ var height = screen.height;
 
 var containers = document.querySelectorAll('.container');
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Get the elements with the class "container"
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Get the elements with the class "container"
   
 
-  // Check if any element was found
-  if (containers.length > 0) {
-    // Set the color for each container
-    containers.forEach(function(container) {
-      container.style.backgroundColor = 'rgba(0, 0, 0, 0.55)';
-    });
-  } else {
-    console.error('No element with class "container" found on the page.');
-  }
-});
+//   // Check if any element was found
+//   if (containers.length > 0) {
+//     // Set the color for each container
+//     containers.forEach(function(container) {
+//       container.style.backgroundColor = 'rgba(0, 0, 0, 0.55)';
+//     });
+//   } else {
+//     console.error('No element with class "container" found on the page.');
+//   }
+// });
 
 window.onload = function GetBGImage() {
   var img = new Image();
@@ -97,4 +97,45 @@ function updateBackground() {
 prevBtn.addEventListener('click', updateBackground);
 nextBtn.addEventListener('click', updateBackground);
 
+//DARK AND LIGHT THEMES
+const DarkBtn = document.getElementById("BtnDarkMode");
+const LightBtn = document.getElementById("BtnLightMode");
 
+var DarkModeActive = true;
+
+DarkBtn.addEventListener('click', function(){
+  DarkModeActive = true;
+  if(DarkModeActive == true){
+    alert("Dark Mode Active");
+    document.getElementById("Link").style.color = "black";
+    document.getElementById("Link").style.border = "solid";
+    document.getElementById("Link").style.border.color = "black";
+    document.getElementById("txt").style.color = "black";
+    document.getElementById("txtbx-1").style.color = "black";
+    document.getElementById("txtbx-1").style.border = "solid";
+    document.getElementById("txtbx-1").style.border.color = "black";
+    document.getElementById("moon").style.fill = "black";
+    document.getElementById("sun").style.fill = "black";
+    document.getElementById("weather").style.fill = "black";
+    document.getElementById("SearchBarInput").style.fill = "black";
+
+  }
+})
+
+LightBtn.addEventListener('click', function(){
+  DarkModeActive = false;
+  if(DarkModeActive == false){
+    alert("Light Mode Active")
+    document.getElementById("Link").style.color = "white";
+    document.getElementById("Link").style.border = "solid";
+    document.getElementById("Link").style.border.color = "white";
+    document.getElementById("txt").style.color = "white";
+    document.getElementById("txtbx-1").style.color = "white";
+    document.getElementById("txtbx-1").style.border = "solid";
+    document.getElementById("txtbx-1").style.border.color = "white";
+    document.getElementById("moon").style.fill = "white";
+    document.getElementById("sun").style.fill = "white";
+    document.getElementById("weather").style.fill = "white";
+    document.getElementById("SearchBarInput").style.fill = "white";
+  }
+})
