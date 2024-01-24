ae1ddac5fb2ee3160d261fb8f854f6d3506cc0a0
Jack <112323010+JMagDev1@users.noreply.github.com>
2024-01-24 17:07:00 +0000

    Reversed the light and dark themes/ colours.

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-var width = screen.width;
-var height = screen.height;
-
-var containers = document.querySelectorAll('.container');
-
-function AddBackgroundBlurLight(){
-  // Check if any element was found
-  if (containers.length > 0) {
-    // Set the color for each container
-    containers.forEach(function(container) {
-      container.style.backgroundColor = 'rgba(0, 0, 0, 0.55)';
-    });
-  } else {
-    console.error('No element with class "container" found on the page.');
-  }
-}
-
-function AddBackgroundBlurDark(){
-  // Check if any element was found
-  if (containers.length > 0) {
-    // Set the color for each container
-    containers.forEach(function(container) {
-      container.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-    });
-  } else {
-    console.error('No element with class "container" found on the page.');
-  }
-}
-
 window.onload = function GetBGImage() {
   var img = new Image();
   img.src = "https://source.unsplash.com/" + width + "x" + height + "/?" + "landscape&";
@@ -47,11 +18,39 @@ window.onload = function GetBGImage() {
     }
     startTime();
     var DarkModeActive = false;
-    LightMode();
+    DarkMode();
   };
 
 };
 
+var width = screen.width;
+var height = screen.height;
+
+var containers = document.querySelectorAll('.container');
+
+function AddBackgroundBlurLight(){
+  // Check if any element was found
+  if (containers.length > 0) {
+    // Set the color for each container
+    containers.forEach(function(container) {
+      container.style.backgroundColor = 'rgba(255, 255, 255, 0.25)';
+    });
+  } else {
+    console.error('No element with class "container" found on the page.');
+  }
+}
+
+function AddBackgroundBlurDark(){
+  // Check if any element was found
+  if (containers.length > 0) {
+    // Set the color for each container
+    containers.forEach(function(container) {
+      container.style.backgroundColor = 'rgba(0, 0, 0, 0.55)';
+    });
+  } else {
+    console.error('No element with class "container" found on the page.');
+  }
+}
 
 function startTime(){
   const today = new Date();
@@ -125,69 +124,9 @@ LightBtn.addEventListener('click', function(){
 })
 
 function DarkMode(){
-  document.getElementById("BtnDarkMode").style.backgroundColor = 'rgba(0, 0, 0, 0.55)';
+  document.getElementById("BtnDarkMode").style.backgroundColor = 'rgba(255, 255, 255, 0.25)';
   document.getElementById("BtnDarkMode").style.borderRadius = '25%';
   document.getElementById("BtnLightMode").style.backgroundColor = 'transparent';
-  document.getElementById("Link").style.color = "black";
-  document.getElementById("Link").style.border = "solid";
-  document.getElementById("Link").style.border.color = "black";
-  document.getElementById("Link1").style.color = "black";
-  document.getElementById("Link1").style.border = "solid";
-  document.getElementById("Link1").style.border.color = "black";
-  document.getElementById("Link2").style.color = "black";
-  document.getElementById("Link2").style.border = "solid";
-  document.getElementById("Link2").style.border.color = "black";
-  document.getElementById("Link3").style.color = "black";
-  document.getElementById("Link3").style.border = "solid";
-  document.getElementById("Link3").style.border.color = "black";
-  document.getElementById("Link4").style.color = "black";
-  document.getElementById("Link4").style.border = "solid";
-  document.getElementById("Link4").style.border.color = "black";
-  document.getElementById("Link5").style.color = "black";
-  document.getElementById("Link5").style.border = "solid";
-  document.getElementById("Link5").style.border.color = "black";
-  document.getElementById("Link6").style.color = "black";
-  document.getElementById("Link6").style.border = "solid";
-  document.getElementById("Link6").style.border.color = "black";
-  document.getElementById("Link7").style.color = "black";
-  document.getElementById("Link7").style.border = "solid";
-  document.getElementById("Link7").style.border.color = "black";
-  document.getElementById("Link8").style.color = "black";
-  document.getElementById("Link8").style.border = "solid";
-  document.getElementById("Link8").style.border.color = "black";
-  document.getElementById("Link9").style.color = "black";
-  document.getElementById("Link9").style.border = "solid";
-  document.getElementById("Link9").style.border.color = "black";
-  document.getElementById("Link10").style.color = "black";
-  document.getElementById("Link10").style.border = "solid";
-  document.getElementById("Link10").style.border.color = "black";
-  document.getElementById("Link11").style.color = "black";
-  document.getElementById("Link11").style.border = "solid";
-  document.getElementById("Link11").style.border.color = "black";
-  document.getElementById("Link12").style.color = "black";
-  document.getElementById("Link12").style.border = "solid";
-  document.getElementById("Link12").style.border.color = "black";
-  document.getElementById("Link13").style.color = "black";
-  document.getElementById("Link13").style.border = "solid";
-  document.getElementById("Link13").style.border.color = "black";
-  document.getElementById("Link14").style.color = "black";
-  document.getElementById("Link14").style.border = "solid";
-  document.getElementById("Link14").style.border.color = "black";
-  document.getElementById("Link15").style.color = "black";
-  document.getElementById("Link15").style.border = "solid";
-  document.getElementById("Link15").style.border.color = "black";
-
-  document.getElementById("txt").style.color = "black";
-
-  document.getElementById("SearchBarInput").style.color = "black"
-  document.getElementById("SearchBarInput").style.border.color = "black";
-  AddBackgroundBlurDark();
-}
-
-function LightMode(){
-  document.getElementById("BtnLightMode").style.backgroundColor = 'rgba(255, 255, 255, 0.25)';
-  document.getElementById("BtnLightMode").style.borderRadius = '25%';
-  document.getElementById("BtnDarkMode").style.backgroundColor = 'transparent';
   document.getElementById("Link").style.color = "white";
   document.getElementById("Link").style.border = "solid";
   document.getElementById("Link").style.border.color = "white";
@@ -241,5 +180,65 @@ function LightMode(){
 
   document.getElementById("SearchBarInput").style.color = "white"
   document.getElementById("SearchBarInput").style.border.color = "white";
+  AddBackgroundBlurDark();
+}
+
+function LightMode(){
+  document.getElementById("BtnLightMode").style.backgroundColor = 'rgba(0, 0, 0, 0.55)';
+  document.getElementById("BtnLightMode").style.borderRadius = '25%';
+  document.getElementById("BtnDarkMode").style.backgroundColor = 'transparent';
+  document.getElementById("Link").style.color = "black";
+  document.getElementById("Link").style.border = "solid";
+  document.getElementById("Link").style.border.color = "black";
+  document.getElementById("Link1").style.color = "black";
+  document.getElementById("Link1").style.border = "solid";
+  document.getElementById("Link1").style.border.color = "black";
+  document.getElementById("Link2").style.color = "black";
+  document.getElementById("Link2").style.border = "solid";
+  document.getElementById("Link2").style.border.color = "black";
+  document.getElementById("Link3").style.color = "black";
+  document.getElementById("Link3").style.border = "solid";
+  document.getElementById("Link3").style.border.color = "black";
+  document.getElementById("Link4").style.color = "black";
+  document.getElementById("Link4").style.border = "solid";
+  document.getElementById("Link4").style.border.color = "black";
+  document.getElementById("Link5").style.color = "black";
+  document.getElementById("Link5").style.border = "solid";
+  document.getElementById("Link5").style.border.color = "black";
+  document.getElementById("Link6").style.color = "black";
+  document.getElementById("Link6").style.border = "solid";
+  document.getElementById("Link6").style.border.color = "black";
+  document.getElementById("Link7").style.color = "black";
+  document.getElementById("Link7").style.border = "solid";
+  document.getElementById("Link7").style.border.color = "black";
+  document.getElementById("Link8").style.color = "black";
+  document.getElementById("Link8").style.border = "solid";
+  document.getElementById("Link8").style.border.color = "black";
+  document.getElementById("Link9").style.color = "black";
+  document.getElementById("Link9").style.border = "solid";
+  document.getElementById("Link9").style.border.color = "black";
+  document.getElementById("Link10").style.color = "black";
+  document.getElementById("Link10").style.border = "solid";
+  document.getElementById("Link10").style.border.color = "black";
+  document.getElementById("Link11").style.color = "black";
+  document.getElementById("Link11").style.border = "solid";
+  document.getElementById("Link11").style.border.color = "black";
+  document.getElementById("Link12").style.color = "black";
+  document.getElementById("Link12").style.border = "solid";
+  document.getElementById("Link12").style.border.color = "black";
+  document.getElementById("Link13").style.color = "black";
+  document.getElementById("Link13").style.border = "solid";
+  document.getElementById("Link13").style.border.color = "black";
+  document.getElementById("Link14").style.color = "black";
+  document.getElementById("Link14").style.border = "solid";
+  document.getElementById("Link14").style.border.color = "black";
+  document.getElementById("Link15").style.color = "black";
+  document.getElementById("Link15").style.border = "solid";
+  document.getElementById("Link15").style.border.color = "black";
+
+  document.getElementById("txt").style.color = "black";
+
+  document.getElementById("SearchBarInput").style.color = "black"
+  document.getElementById("SearchBarInput").style.border.color = "black";
   AddBackgroundBlurLight();  
 }
